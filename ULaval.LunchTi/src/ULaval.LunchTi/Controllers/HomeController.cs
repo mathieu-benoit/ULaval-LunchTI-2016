fd9e8feafa7a75a1ceee942c418c08a0e04c7fa5
chaaboah <ahmed.chaabouni@nurun.com>
2016-03-08 18:46:39 -0500

    Configure the project to use ASP.NET MVC 6

---
+++ ULaval.LunchTi/src/ULaval.LunchTi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ULaval.LunchTi.Models;
+
+namespace ULaval.LunchTi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new HomeModel() { Title = "Hello, World !" });
+        }
+    }
+}
--- ULaval.LunchTi/src/ULaval.LunchTi/Startup.cs
+++ ULaval.LunchTi/src/ULaval.LunchTi/Startup.cs
@@ -15,6 +15,7 @@ namespace ULaval.LunchTi
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,10 +23,12 @@ namespace ULaval.LunchTi
         {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc(routes =>
+            routes.MapRoute(name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}"));
+
+
+
         }
 
         // Entry point for the application.
