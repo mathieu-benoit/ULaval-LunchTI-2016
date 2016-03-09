8baa57a00d5ec319d2e0f324ff6e5b949a18bb55
chaaboah <ahmed.chaabouni@nurun.com>
2016-03-08 19:19:55 -0500

    Add of the DeveloperExceptionPage

--- ULaval.LunchTi/src/ULaval.LunchTi/Startup.cs
+++ ULaval.LunchTi/src/ULaval.LunchTi/Startup.cs
@@ -25,6 +25,7 @@ namespace ULaval.LunchTi
         {
             app.UseIISPlatformHandler();
             app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
             app.UseMvc(routes =>
             routes.MapRoute(name: "default",
             template: "{controller=Home}/{action=Index}/{id?}"));
