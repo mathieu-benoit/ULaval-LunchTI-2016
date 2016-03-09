266f5151b256c178b114e6ab1ed1cbf476af4770
mathieu-benoit <mathieu-benoit@hotmail.fr>
2016-03-09 15:08:49 -0500

    Add xUnit Tests project to the solution.

---
+++ ULaval.LunchTi/src/ULaval.LunchTi.Tests/SimpleTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace ULaval.LunchTi.Tests
+{
+    public class SimpleTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.NotEqual(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
