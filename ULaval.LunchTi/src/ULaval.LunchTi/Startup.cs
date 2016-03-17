6f8481095f6a737ae166a248e076c539a0e3b261
mathieu-benoit <mathieu-benoit@hotmail.fr>
2016-03-17 11:16:35 -0400

    Setup appsettings.json to allow settings per env.

--- ULaval.LunchTi/src/ULaval.LunchTi/Startup.cs
+++ ULaval.LunchTi/src/ULaval.LunchTi/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.Data.Entity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ULaval.LunchTi.Models;
 using ULaval.LunchTi.Services;
@@ -10,6 +11,18 @@ namespace ULaval.LunchTi
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -19,7 +32,7 @@ namespace ULaval.LunchTi
             services.AddEntityFramework()
                 .AddSqlServer()
                 .AddDbContext<ULavalLunchTiContext>(options =>
-                    options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ULavalLunchTiContext-0c8ee4b7-1e61-4f48-a290-46160646484d;Trusted_Connection=True;MultipleActiveResultSets=true"));
+                    options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
 
             services.AddSwaggerGen();
             services.AddGlimpse();
